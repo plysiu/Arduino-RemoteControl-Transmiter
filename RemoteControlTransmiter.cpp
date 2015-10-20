2da17265892314ad59a7ebb233ccb9c548616a7e
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-10-20 16:25:09 +0200

    Removed serial messages added lcd display

--- RemoteControlTransmiter.cpp
+++ RemoteControlTransmiter.cpp
@@ -2,18 +2,20 @@
 #include "RemoteControlTransmiter.h"
 #include <Signal.h>
 #include <Button.h>
-
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x20 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 Signal a = Signal(A0);
 Signal b = Signal(A1);
+
 Signal c = Signal(A2);
 Signal d = Signal(A3);
 
-Button calibrate = Button(7, PULLUP);
+Button button1 = Button(9, PULLUP);
+Button button2 = Button(7, PULLUP);
+Button button3 = Button(5, PULLUP);
 
 void setup() {
 	Serial.begin(9600);
@@ -21,9 +23,34 @@ void setup() {
 	lcd.backlight();
 }
 
+void disply() {
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print(a.getTiltRod());
+	lcd.setCursor(4, 0);
+	lcd.print(":");
+	lcd.print(b.getTiltRod());
+
+	lcd.setCursor(10, 0);
+	lcd.print("A:");
+	lcd.print(button1.isPressed());
+
+	lcd.setCursor(0, 1);
+	lcd.print(c.getTiltRod());
+	lcd.setCursor(4, 1);
+	lcd.print(":");
+	lcd.print(d.getTiltRod());
+	lcd.setCursor(10, 1);
+
+	lcd.print("B:");
+	lcd.print(!button2.isPressed());
+	lcd.print("C:");
+	lcd.print(button3.isPressed());
+}
+
 void loop() {
 
-	if (calibrate.isPressed()) {
+	if (button1.isPressed()) {
 		Signal::setCalibrationOn();
 	} else {
 		Signal::setCalibrationOff();
@@ -33,56 +60,7 @@ void loop() {
 	b.read();
 	c.read();
 	d.read();
+	disply();
+	delay(75);
 
-	lcd.setCursor(0,0);
-	lcd.print(a.getTiltRod());
-	lcd.setCursor(0,4);
-	lcd.print(b.getTiltRod());
-	lcd.setCursor(1,0);
-	lcd.print(c.getTiltRod());
-	lcd.setCursor(1,4);
-	lcd.print(d.getTiltRod());
-
-
-
-	Serial.print(a.getMinValue());
-	Serial.print("-");
-	Serial.print(a.getCenterValue());
-	Serial.print("-");
-	Serial.print(a.getMaxValue());
-	Serial.print("-");
-	Serial.print(a.getTiltRod());
-	Serial.print("-");
-	Serial.println(a.getValue());
-
-	Serial.print(b.getMinValue());
-	Serial.print("-");
-	Serial.print(b.getCenterValue());
-	Serial.print("-");
-	Serial.print(b.getMaxValue());
-	Serial.print("-");
-	Serial.print(b.getTiltRod());
-	Serial.print("-");
-	Serial.println(b.getValue());
-
-	Serial.print(c.getMinValue());
-	Serial.print("-");
-	Serial.print(c.getCenterValue());
-	Serial.print("-");
-	Serial.print(c.getMaxValue());
-	Serial.print("-");
-	Serial.print(c.getTiltRod());
-	Serial.print("-");
-	Serial.println(c.getValue());
-
-	Serial.print(d.getMinValue());
-	Serial.print("-");
-	Serial.print(d.getCenterValue());
-	Serial.print("-");
-	Serial.print(d.getMaxValue());
-	Serial.print("-");
-	Serial.print(d.getTiltRod());
-	Serial.print("-");
-	Serial.println(d.getValue());
-	delay(2000);
 }
